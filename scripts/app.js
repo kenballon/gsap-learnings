58e4992e6afea2fdf68476464d2f1d04fbd039f2
Ken Ballon <bkenn003@gmail.com>
2023-10-19 21:49:04 +0800

    update gsap

--- scripts/app.js
+++ scripts/app.js
@@ -1,46 +1,31 @@
-// gsap.to(".square", {
-//   x: 400,
-//   ease: "elastic",
-//   opacity: 1,
-//   duration: 2,
-//   repeat: -1,
-//   yoyo: true,
-// });
-// gsap.to(".square-pink", {
-//   x: 400,
-//   ease: "elastic",
-//   opacity: 1,
-//   duration: 1,
-//   repeat: -1,
-//   yoyo: true,
-// });
+document.addEventListener("readystatechange", (e) => {
+  if (e.target.readyState === "complete") {
+    const headingTitle = document.getElementById("headingH1");
+    //   const textHeadingVal = headingTitle.textContent.trim();
+    //   const textValArr = textHeadingVal.split("");
 
-window.addEventListener("DOMContentLoaded", () => {
-  const headingTitle = document.getElementById("headingH1");
-  const textHeadingVal = headingTitle.textContent.trim();
-  const textValArr = textHeadingVal.split("");
+    //   headingTitle.innerHTML = "";
 
-  headingTitle.innerHTML = "";
+    //   textValArr.forEach((char) => {
+    //     let span = document.createElement("div");
+    //     span.classList.add("staggered-char");
+    //     span.style.display = "inline-block";
+    //     span.innerHTML = char;
 
-  textValArr.forEach((char) => {
-    let span = document.createElement("div");
-    span.classList.add("staggered-char");
-    span.style.display = "inline-block";
-    span.innerHTML = char;
+    //     headingTitle.appendChild(span);
+    //   });
 
-    headingTitle.appendChild(span);
-  });
+    const textHeading = new SplitType("#headingH1");
 
-  // const textHeading = new SplitType("#headingH1");
+    gsap.fromTo(
+      ".squares .square",
+      { y: -100, opacity: 0 },
+      { y: 0, stagger: 0.5, opacity: 1, ease: "back.out(2.3)" }
+    );
+    gsap.from(
+      textHeading.words,
 
-  gsap.fromTo(
-    ".squares .square",
-    { y: -100, opacity: 0 },
-    { y: 0, stagger: 0.5, opacity: 1, ease: "back.out(2.3)" }
-  );
-  gsap.from(
-    ".staggered-char",
-
-    { y: "100%", stagger: 0.1, opacity: 1 }
-  );
+      { y: "100%", stagger: 0.1, opacity: 1 }
+    );
+  }
 });
